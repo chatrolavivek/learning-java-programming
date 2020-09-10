1d73ea56d00efd2729b328d85ae5977cf19dfdeb
LAPTOP-VIVEK\chatr <chatrolavivek@gmail.com>
2020-09-10 14:08:39 -0400

    ## javac and java practice in cmd

---
+++ JavaPrograms/src/practice/CommandPromtPractice.java
@@ -0,0 +1,16 @@
+package practice;
+
+public class CommandPromtPractice {
+	public static void main(String[] args) {
+		System.out.println("Class CommandPromtPractice");
+	}
+}
+
+class A {
+	public static void main(String[] args) {
+		System.out.println("Class A");
+	}
+}
+
+class B {	
+}
