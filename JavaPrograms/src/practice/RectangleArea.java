eea7db6f51226410d8805c75de565b15aee35cdd
LAPTOP-VIVEK\chatr <chatrolavivek@gmail.com>
2020-09-11 17:18:17 -0400

    ## Program to calculate the area of a rectangle

---
+++ JavaPrograms/src/practice/RectangleArea.java
@@ -0,0 +1,24 @@
+package practice;
+
+import java.util.Scanner;
+
+public class RectangleArea {
+
+	public static void main(String[] args) {
+
+		Scanner s = new Scanner(System.in);
+
+		System.out.print("Enter the length in mm: ");
+		double length = s.nextDouble();
+
+		System.out.println("Enter the breadth in mm: ");
+		double breadth = s.nextDouble();
+
+		double areaOfRectangle = length * breadth;
+		System.out.println("Area of the Rectange is: " + areaOfRectangle + " mm2");
+
+		s.close();
+
+	}
+
+}
