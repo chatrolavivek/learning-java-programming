b06e4c0e755a4964eee8bd637916fbbd7b47ee89
LAPTOP-VIVEK\chatr <chatrolavivek@gmail.com>
2020-09-15 08:22:20 -0400

    ## added code to show the demo of break loop

---
+++ JavaPrograms/src/practice/BreakDemo.java
@@ -0,0 +1,28 @@
+package practice;
+
+import java.util.Scanner;
+
+public class BreakDemo {
+
+	public static void main(String[] args) {
+
+//		WAP to accept the numbers until the user enter zero and find the sum of these 
+//		given numbers
+
+		Scanner s = new Scanner(System.in);
+		int n, sum = 0;
+
+		while (true) {
+			System.out.println("Enter a number:");
+			n = s.nextInt();
+			if (n == 0) {
+				break;
+			}
+			sum = sum + n;
+		}
+		System.out.println("Sum: " + sum);
+
+		s.close();
+	}
+
+}
