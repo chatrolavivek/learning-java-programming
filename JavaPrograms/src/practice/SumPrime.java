3701236693c1c552ef37e70aa866736f9e27edf6
LAPTOP-VIVEK\chatr <chatrolavivek@gmail.com>
2020-09-15 08:28:52 -0400

    ## added code to find sum of prime numbers from 2 to 100

---
+++ JavaPrograms/src/practice/SumPrime.java
@@ -0,0 +1,18 @@
+package practice;
+
+public class SumPrime {
+
+	public static void main(String[] args) {
+
+//		WAP to find sum of prime numbers from 2 to 100
+
+		int sum = 0;
+		for (int i = 2; i <= 100; i++) {
+			if (PrimeOrNot.prime(i)) {
+				sum = sum + i;
+			}
+		}
+		System.out.println("Sum of Prime numbers [2-100] = " + sum);
+	}
+
+}
