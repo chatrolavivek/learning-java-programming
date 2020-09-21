6ae3ae52f71109b783f71245a570fbb364385aca
LAPTOP-VIVEK\chatr <chatrolavivek@gmail.com>
2020-09-21 17:59:35 -0400

    ## created class StudentDemo to test the functionality of Student class

---
+++ JavaPrograms/src/oop/assignment/StudentDemo.java
@@ -0,0 +1,14 @@
+package oop.assignment;
+
+public class StudentDemo {
+
+	public static void main(String[] args) {
+
+		// Tests the functionality of Student class
+
+		Student stud1 = new Student(40059267, "Vivek Chatrola", 89, 76, 95);
+		stud1.calculateTotal();
+		stud1.displayStudDetails();
+	}
+
+}
