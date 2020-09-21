b8156983a35e90adb017ef58f279bd02d48f334a
LAPTOP-VIVEK\chatr <chatrolavivek@gmail.com>
2020-09-21 18:03:52 -0400

    ## added program to display menu repeatedly until the user chooses Exit

---
+++ JavaPrograms/src/com/account/Account.java
@@ -0,0 +1,41 @@
+package com.account;
+
+public class Account {
+
+	private int acctId, acctBalance;
+	private String acctType;
+
+	// Set the values to these attributes
+	public Account(int accountId, String accountType, int accountBalance) {
+		this.acctId = accountId;
+		this.acctType = accountType;
+		this.acctBalance = accountBalance;
+	}
+
+	// Default Constructor
+	public Account() {
+	}
+
+	// Gets balance
+	public int getBalance() {
+		return acctBalance;
+	}
+
+	// Adds given amount to the available balance
+	public void deposit(int depositAmount) {
+		acctBalance += depositAmount;
+	}
+
+	// Subtracts the given amount from the available balance
+	public void withdraw(int withdrawAmount) {
+		acctBalance -= withdrawAmount;
+	}
+
+	// Displays accountNo, accountType, accountBalance. Create a class Main1 which
+	public void dispAccountDetails() {
+		System.out.println("Account ID: " + acctId);
+		System.out.println("Account Type: " + acctType);
+		System.out.println("Account Balance: " + acctBalance);
+	}
+
+}
---
+++ JavaPrograms/src/com/account/test/AccountMain.java
@@ -0,0 +1,59 @@
+package com.account.test;
+
+import java.util.Scanner;
+
+import com.account.Account;
+
+public class AccountMain {
+
+	public static void main(String[] args) {
+		// Tests com.account.account class
+
+		Scanner s = new Scanner(System.in);
+
+		System.out.println("Enter Account ID: ");
+		int accountId = s.nextInt();
+		s.nextLine();
+		System.out.println("Enter Accont Type: ");
+		String accountType = s.nextLine();
+		System.out.println("Enter Account Balance: ");
+		int accountBalance = s.nextInt();
+
+		Account userAcc = new Account(accountId, accountType, accountBalance);
+
+		int userChoice, depositAmount, withdrawAmount;
+		do {
+			System.out.println("\nWhat would you like to do today?");
+			System.out.println("1. Deposit");
+			System.out.println("2. Withdraw");
+			System.out.println("3. Display Balance");
+			System.out.println("4. Exit\n");
+
+			userChoice = s.nextInt();
+
+			switch (userChoice) {
+			case 1:
+				System.out.println("Enter the amount to be deposited: ");
+				depositAmount = s.nextInt();
+				userAcc.deposit(depositAmount);
+				break;
+			case 2:
+				System.out.println("Enter the amount to be withdrawn: ");
+				withdrawAmount = s.nextInt();
+				userAcc.withdraw(withdrawAmount);
+				break;
+			case 3:
+				System.out.println(userAcc.getBalance());
+				break;
+			case 4:
+				System.out.println("\nThank you for banking with us today :)");
+				System.exit(1);
+			default:
+				break;
+			}
+		} while (userChoice != 4);
+
+		s.close();
+	}
+
+}
