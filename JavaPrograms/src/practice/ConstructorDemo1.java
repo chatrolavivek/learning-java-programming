180b3b716101849c742ee638373fc5dedcd649b8
LAPTOP-VIVEK\chatr <chatrolavivek@gmail.com>
2020-09-20 22:25:44 -0400

    ## added code for constructor demo

---
+++ JavaPrograms/src/practice/ConstructorDemo1.java
@@ -0,0 +1,24 @@
+package practice;
+
+class Sample
+{
+	int i;
+	int j;
+	Sample(int i,int j)//constructor
+	{
+		this.i = i;
+		this.j = j;
+	}
+	void display()
+	{
+		System.out.println("i="+i+" "+"j="+j);
+	}
+}
+public class ConstructorDemo1 {
+	public static void main(String[] args) {
+		Sample s = new Sample(10,20);
+		s.display();//i=10 j=20
+
+	}
+
+}
