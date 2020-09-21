0c57a5ed92c1208113d663946c0893d7ac2ae258
LAPTOP-VIVEK\chatr <chatrolavivek@gmail.com>
2020-09-20 22:25:51 -0400

    ## added code for constructor demo

---
+++ JavaPrograms/src/practice/ConstructorDemo2.java
@@ -0,0 +1,34 @@
+package practice;
+
+class Item
+{
+	int itemNum;
+	String itemName;
+	Item(int itemNum,String itemName)//parameterized constructor
+	{
+		this.itemNum = itemNum;
+		this.itemName = itemName;
+	}
+	Item(){}//default constructor
+	void display()
+	{
+		System.out.println("Item Num = "+itemNum);
+		System.out.println("Item Name = "+itemName);
+	}
+}
+public class ConstructorDemo2 {
+	public static void main(String[] args) {
+		Item item1 = new Item(111,"Monitor");
+		item1.display();
+		Item item2 = new Item();
+		item2.display();
+	}
+}
+/*
+output
+------
+Item Num = 111
+Item Name = Monitor
+Item Num = 0
+Item Name = null
+*/
