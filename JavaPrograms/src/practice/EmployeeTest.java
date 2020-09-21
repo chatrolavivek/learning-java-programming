cbef7588cbd1d3c5590ade90ad61d7a8e0222950
LAPTOP-VIVEK\chatr <chatrolavivek@gmail.com>
2020-09-20 22:27:07 -0400

    ## ## added code for this keyword demo

---
+++ JavaPrograms/src/practice/EmployeeTest.java
@@ -0,0 +1,30 @@
+package practice;
+
+class Employee {
+	int empNum;
+	String empName;
+	double empSalary;
+
+	void setEmpDetails(int empNum, String empName, double empSalary) {
+		this.empNum = empNum;
+		this.empName = empName;
+		this.empSalary = empSalary;
+	}
+
+	void dispEmpDetails() {
+		System.out.println("Emp Num = " + empNum);
+		System.out.println("Emp Name = " + empName);
+		System.out.println("Emp Salary = " + empSalary);
+	}
+}
+
+public class EmployeeTest {
+	public static void main(String[] args) {
+		Employee e1 = new Employee();
+		e1.setEmpDetails(111, "Raj", 8000.0);
+		e1.dispEmpDetails();
+		Employee e2 = new Employee();
+		e2.setEmpDetails(222, "Ramu", 6000.0);
+		e2.dispEmpDetails();
+	}
+}
