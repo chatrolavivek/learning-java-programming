b3927b300c152320ffe05a74ebaa5d72351789b8
LAPTOP-VIVEK\chatr <chatrolavivek@gmail.com>
2020-09-21 18:04:43 -0400

    ## Tested Customer and Account class

---
+++ JavaPrograms/src/oop/assignment/AccountTest.java
@@ -0,0 +1,11 @@
+package oop.assignment;
+
+public class AccountTest {
+
+	// Tests the Customer and Account class
+	public static void main(String[] args) {
+		Account1 custAcc = new Account1(724690, "Saving", 15000);
+		custAcc.dispAccountDetails(49, "Vivek Chatrola", "123 Parc Drive, Boston, MA 235490");
+	}
+
+}
