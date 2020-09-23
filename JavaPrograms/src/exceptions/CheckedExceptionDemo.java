d9dc9b5c28707bbd0c1a09eae667b3ec61aebc10
LAPTOP-VIVEK\chatr <chatrolavivek@gmail.com>
2020-09-23 01:29:39 -0400

    ## added program for the demo of checked exception

---
+++ JavaPrograms/src/exceptions/CheckedExceptionDemo.java
@@ -0,0 +1,19 @@
+package exceptions;
+
+import java.io.IOException;
+
+public class CheckedExceptionDemo {
+
+	public static void main(String[] args) throws IOException {
+
+//		try {
+//			int n = System.in.read();
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+
+		int n = System.in.read();
+	}
+
+}
