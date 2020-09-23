51eda2599e50d4ca3f3e6ec9c41716ef723f0b01
LAPTOP-VIVEK\chatr <chatrolavivek@gmail.com>
2020-09-23 01:32:08 -0400

    ## added program for the demo of Math.random()

---
+++ JavaPrograms/src/practice/RandomDemo.java
@@ -0,0 +1,11 @@
+package practice;
+
+public class RandomDemo {
+
+	public static void main(String[] args) {
+
+		// Random numbers are used to generate OTPs (One Time Password)
+
+		System.out.println((int) (1000000 * Math.random()));
+	}
+}
